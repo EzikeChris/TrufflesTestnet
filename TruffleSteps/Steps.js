6bdf361b17b9e3bcf9a392d0abd029730a9763b8
ezikechris <christophermbe1009@gmail.com>
2021-04-06 17:21:42 +0100

    first commit

---
+++ TruffleSteps/Steps.js
@@ -0,0 +1,26 @@
+
+//// initialis truffle with its containing folders
+truffle init 
+
+
+//// create truffle migrations
+
+truffle create migration contract name
+
+
+
+//// configure migration file for deployment
+
+configure / modify your migration file
+
+//// Compile your contract into byecode for evn and produce abi your migration for 
+
+truffle compile
+
+/// After Compiling  testnet should be initialised contract make use of the truffle develop command
+truffle develop
+
+/// Run Migrate 
+
+
+Migrate 
